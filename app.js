b2ad0bbf8f652ad55a9a6511bd039fba2be47ff5
Derrick <oseiagyemanb@gmail.com>
2023-12-24 21:48:40 +0000

    update of app.js file

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('E-Commerce API');
+//dotenv import
+require('dotenv').config();
+require('express-async-errors');
+
+//express package
+const express = require('express');
+const app = express();
+
+//auxillary packages
+const morgan = require('morgan');
+
+//database import
+const connectDB = require('./db/connect');
+
+//middleware import
+const notFoundMiddleware = require('./middleware/not-found');
+const noterrorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('e-commerce api');
+});
+
+app.use(notFoundMiddleware);
+app.use(noterrorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URL);
+		app.listen(port, console.log(`Server is listening on port ${port}...`));
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
