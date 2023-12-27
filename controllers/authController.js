b6103c2f9c8e7ef87dac7bed13d8095746aa8a25
Derrick <oseiagyemanb@gmail.com>
2023-12-27 21:49:57 +0000

    update of authController file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,24 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const { createJWT } = require('../utils');
+
 const register = async (req, res) => {
-	res.send('register user');
+	const { email, name, password } = req.body;
+	const emailAlreadyExist = await User.findOne({ email });
+	if (emailAlreadyExist) {
+		throw new CustomError.BadRequestError('Email already exists');
+	}
+
+	// first registered user is an admin
+	const isFirstAccount = (await User.countDocuments({})) === 0;
+	const role = isFirstAccount ? 'admin' : 'user';
+
+	const user = await User.create({ email, name, password, role });
+	const tokenUser = { name: user.name, userId: user._id, role: user.role };
+	const token = createJWT({ payload: tokenUser });
+
+	res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 const login = async (req, res) => {
 	res.send('login user');
