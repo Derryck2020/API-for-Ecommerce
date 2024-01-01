60edc7752f09a9073b7b1dd6d5df411b9b0fb205
Derrick <oseiagyemanb@gmail.com>
2024-01-01 22:11:50 +0000

    update of authController file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const { createJWT } = require('../utils');
+const { attachCookiesToReponse } = require('../utils');
 
 const register = async (req, res) => {
 	const { email, name, password } = req.body;
@@ -16,15 +16,39 @@ const register = async (req, res) => {
 
 	const user = await User.create({ email, name, password, role });
 	const tokenUser = { name: user.name, userId: user._id, role: user.role };
-	const token = createJWT({ payload: tokenUser });
+	attachCookiesToReponse({ res, user: tokenUser });
+	res.status(StatusCodes.CREATED).json({ user: tokenUser });
+};
 
-	res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
-};
 const login = async (req, res) => {
-	res.send('login user');
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		throw new CustomError.BadRequestError(
+			'Please provide email and password'
+		);
+	}
+	const user = await User.findOne({ email });
+
+	if (!user) {
+		throw new CustomError.UnauthenticatedError('Invalid Credentials');
+	}
+	const isPasswordCorrect = await user.comparePassword(password);
+	if (!isPasswordCorrect) {
+		throw new CustomError.UnauthenticatedError('Invalid Credentials');
+	}
+
+	const tokenUser = { name: user.name, userId: user._id, role: user.role };
+	attachCookiesToReponse({ res, user: tokenUser });
+	res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
+
 const logout = async (req, res) => {
-	res.send('logout user');
+	res.cookie('token', 'logout', {
+		httpOnly: true,
+		expires: new Date(Date.now()),
+	});
+	res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 };
 
 module.exports = {
