5f515c8e923b37ac565fb07175046fec24ddf4a8
Derrick <oseiagyemanb@gmail.com>
2024-01-15 19:52:24 +0000

    update of file

--- models/Review.js
+++ models/Review.js
@@ -34,8 +34,37 @@ const ReviewSchema = mongoose.Schema(
 
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
-ReviewSchema.statics.calculateAverageRating = async function () {
-	console.log(productId);
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+	const result = await this.aggregate([
+		{
+			$match: {
+				product: productId,
+			},
+		},
+		{
+			$group: {
+				_id: null,
+				averageRating: {
+					$avg: '$rating',
+				},
+				numOfReviews: {
+					$sum: 1,
+				},
+			},
+		},
+	]);
+	console.log(SpeechRecognitionResultList);
+	try {
+		await this.model('Product').findOneAndUpdate(
+			{ _id: productId },
+			{
+				averageRating: Math.ceil(result[0]?.averageRating || 0),
+				numOfReviews: result[0]?.numOfReviews || 0,
+			}
+		);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 ReviewSchema.post('save', async function () {
