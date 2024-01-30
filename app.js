eea90e2633877a7fea6275afc698794fb957fc82
Derrick <oseiagyemanb@gmail.com>
2024-01-30 22:51:40 +0000

    update of file

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 //auxillary packages
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
 
 //database import
 const connectDB = require('./db/connect');
@@ -17,6 +18,8 @@ const connectDB = require('./db/connect');
 const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
 const productRouter = require('./routes/productRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const orderRouter = require('./routes/orderRoutes');
 
 //middleware import
 const notFoundMiddleware = require('./middleware/not-found');
@@ -26,6 +29,9 @@ app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
+app.use(express.static('./public'));
+app.use(fileUpload());
+
 app.get('/', (req, res) => {
 	res.send('e-commerce api development');
 });
@@ -33,6 +39,8 @@ app.get('/', (req, res) => {
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/orders', orderRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
